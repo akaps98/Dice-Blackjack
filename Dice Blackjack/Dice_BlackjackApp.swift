c428b78d794d7bef23b6095e5ad195411101c9b7
Tony <fc0302@naver.com>
2023-08-11 11:31:54 +0700

    Initial Commit

---
+++ Dice Blackjack/Dice_BlackjackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_BlackjackApp.swift
+//  Dice Blackjack
+//
+//  Created by Tony on 2023/08/11.
+//
+
+import SwiftUI
+
+@main
+struct Dice_BlackjackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
